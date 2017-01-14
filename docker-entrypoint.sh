cc67eaac8473f3fe58c93ca7738129e2c2748e33
rafaeltuelho <rafaelcba@gmail.com>
2017-01-13 21:44:54 -0300

    fixing issue #5...

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,24 +3,27 @@ set -e
 
 if [ "$1" = 'start' ]; then
 
+  #get the current ip addr of the continer
+  CONTAINER_IP_ADDR=$(/sbin/ip route|awk '/default/ { print $3 }')
 	MODCLUSTER_PORT=${MODCLUSTER_PORT:-6666}
 	MODCLUSTER_ADVERTISE=${MODCLUSTER_ADVERTISE:-On}
 	MODCLUSTER_ADVERTISE_GROUP=${MODCLUSTER_ADVERTISE_GROUP:-224.0.1.105:23364}
 	MODCLUSTER_NET=${MODCLUSTER_NET:-172.}
 	MODCLUSTER_MANAGER_NET=${MODCLUSTER_MANAGER_NET:-$MODCLUSTER_NET}
-	
+
 	echo
 	echo "Starting httpd with mod_cluster"
 	echo "==============================="
 	echo "MODCLUSTER_PORT            ${MODCLUSTER_PORT}"
-	echo "MODCLUSTER_ADVERTISE       ${MODCLUSTER_ADVERTISE}" 
-	echo "MODCLUSTER_ADVERTISE_GROUP ${MODCLUSTER_ADVERTISE_GROUP}" 
-	echo "MODCLUSTER_NET             ${MODCLUSTER_NET}" 
-	echo "MODCLUSTER_MANAGER_NET     ${MODCLUSTER_MANAGER_NET}" 
+	echo "MODCLUSTER_ADVERTISE       ${MODCLUSTER_ADVERTISE}"
+	echo "MODCLUSTER_ADVERTISE_GROUP ${MODCLUSTER_ADVERTISE_GROUP}"
+	echo "MODCLUSTER_NET             ${MODCLUSTER_NET}"
+	echo "MODCLUSTER_MANAGER_NET     ${MODCLUSTER_MANAGER_NET}"
+	echo "mod_cluster server advertise url: http://${CONTAINER_IP_ADDR}:${MODCLUSTER_PORT}"
 	echo
 
 	MOD_CLUSTER_CONF_PATH=${HTTPD_MC_BUILD_DIR}/conf/extra/mod_cluster.conf
-		
+
 	if grep -q FACTORY_DEFAULTS ${MOD_CLUSTER_CONF_PATH}; then
 		# create mod-cluster.conf from environment variables
 		echo "Creating ${MOD_CLUSTER_CONF_PATH} configuration file:"
@@ -36,11 +39,15 @@ MemManagerFile ${HTTPD_MC_BUILD_DIR}/cache/mod_cluster
 
 <IfModule manager_module>
 Listen ${MODCLUSTER_PORT}
-  <VirtualHost *:${MODCLUSTER_PORT}>
+  <VirtualHost ${CONTAINER_IP_ADDR}:${MODCLUSTER_PORT}>
+		ErrorLog logs/mod_cluster_error.log
+		CustomLog logs/mod_cluster.log common
+		LogLevel debug
     <Directory />
       Require ip ${MODCLUSTER_NET}
     </Directory>
-    ServerAdvertise ${MODCLUSTER_ADVERTISE}
+		AllowDisplay On
+    ServerAdvertise ${MODCLUSTER_ADVERTISE} http://${CONTAINER_IP_ADDR}:${MODCLUSTER_PORT}
     AdvertiseGroup ${MODCLUSTER_ADVERTISE_GROUP}
     EnableMCPMReceive
     <Location /mcm>
@@ -54,7 +61,7 @@ EOT
 		cat ${MOD_CLUSTER_CONF_PATH}
 		echo
 	fi
-	
+
 	echo "-- ${HTTPD_MC_BUILD_DIR}/bin/apachectl $@"
 	${HTTPD_MC_BUILD_DIR}/bin/apachectl "$@"
 else
